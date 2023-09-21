3eadc153850a540f4bcfc8d07fe7290d0e2d0e68
mkrtum66 <mkrtumabrahamyan@Mkrtums-MacBook-Pro.local>
2023-09-21 12:51:01 -0700

    add firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTGP4nwRDAfW3sfxOXvtKJ2UT4PsMIzfY",
+  authDomain: "vjglassmoldings.firebaseapp.com",
+  projectId: "vjglassmoldings",
+  storageBucket: "vjglassmoldings.appspot.com",
+  messagingSenderId: "492270754114",
+  appId: "1:492270754114:web:bf71f1359432cb0faa90db",
+  measurementId: "G-FQC7WGSFT0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const projectFirestore = getFirestore(app);
+const projectStorage = getStorage(app)
+
+export {projectFirestore, projectStorage}
+
