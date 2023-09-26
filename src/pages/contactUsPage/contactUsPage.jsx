d25872760513898d1ad26663615096b2752e7487
mkrtum66 <mkrtumabrahamyan@Mkrtums-MacBook-Pro.local>
2023-09-25 22:47:27 -0700

    changes

---
+++ src/pages/contactUsPage/contactUsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './contactUsPage.scss';
+
+import Container from '@mui/material/Container';
+
+const ContactUsPage = () => {
+  return (
+    <div className="contactUsPage">
+      <Container>
+        <h1>Contact Us Page</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default ContactUsPage;
---
+++ src/pages/homepage/homePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homePage.scss';
+
+import Container from '@mui/material/Container';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Container>
+        <h1>Home Page</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
